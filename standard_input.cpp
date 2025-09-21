2d890c1fb087670593fc67726ba6197d832358ed
howyee9527 <piouxcody@gmail.com>
2025-09-21 21:30:27 +0800

    Create standard_input.cpp

---
+++ standard_input.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    //標準化輸出輸入直到EOF，%d可隨題目做變化
+    int a, b;
+    // 使用 scanf 讀取兩個整數直到 EOF
+    while (scanf("%d %d", &a, &b) == 2) {
+        // 使用 printf 輸出兩個整數
+        printf("%d %d\n", a, b);
+    }
+    return 0;
+}
